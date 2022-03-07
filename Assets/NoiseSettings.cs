9070546fa1b54d9069e3628838a16fbb96ea8678
Mario Crippa <mmacros@gmail.com>
2022-03-07 01:41:45 -0300

    Episodio 04
    Multiple Noise Types

--- Assets/NoiseSettings.cs
+++ Assets/NoiseSettings.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings 
 {
-    public float strength = 1;
-    [Range(1,8)]
-    public int numLayers = 1;
-    public float baseRoughness = 1;
-    public float roughness = 2;
-    public float persistence = .5f;
-    public Vector3 centre;
-    public float minValue;
+    public enum FilterType
+    {
+        Simple,
+        Ridgid
+    }
+    public FilterType filterType;
+    // No mostramos en el editor si el filterType distinto de  0, Simple
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    // No mostramos en el editor si el filterType distinto de  1, Ridgid
+    [ConditionalHide("filterType", 1)]
+    public RidgidNoiseSettings ridgidNoiseSettings;
+    
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        public float strength = 1;
+        [Range(1,8)]
+        public int numLayers = 1;
+        public float baseRoughness = 1;
+        public float roughness = 2;
+        public float persistence = .5f;
+        public Vector3 centre;
+        public float minValue;
+    }
+    
+    [System.Serializable]
+    public class RidgidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;        
+    }
+
 }
